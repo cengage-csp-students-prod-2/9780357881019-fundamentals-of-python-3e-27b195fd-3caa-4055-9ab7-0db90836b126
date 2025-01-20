774689604449a7ff9c677ec732910eecc85421b6
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-20 17:48:52 +0000

    Testing Mon Jan 20 2025 17:48:52 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex10/student/employeepay.py
+++ chapter2/ex10/student/employeepay.py
@@ -1,1 +1,5 @@
 # Write your program here
+wage=round(float(input("Tell me the hourly wage")),2)
+reghrs=round(float(input("Tell me the regular hours")),2)
+othrs=round(float(input("Tell me the overtime hours")),2)
+print(f'The weekly pay is ${format(wage*(reghrs+othrs*1.5),".2f")}')
