bb0895b542814fdccf2c042e3af98ce930d1e4ca
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-20 15:46:22 +0000

    Testing Mon Jan 20 2025 15:46:22 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex03/student/fivestar.py
+++ chapter2/ex03/student/fivestar.py
@@ -1,1 +1,4 @@
 # Write your program here
+newv=int(input("How many new videos are being rented?"))
+oldv=int(input("How many old videos are being rented?"))
+print(f"Total cost is ${2*oldv+3*newv}.00")
