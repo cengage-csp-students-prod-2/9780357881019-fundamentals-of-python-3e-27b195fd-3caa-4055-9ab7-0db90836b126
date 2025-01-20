b9d424ee503c7ec5675f2fb1127dc427d4561b70
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-20 17:30:02 +0000

    Testing Mon Jan 20 2025 17:30:02 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex08/student/lightyear.py
+++ chapter2/ex08/student/lightyear.py
@@ -1,3 +1,3 @@
 # Write your program here
 years=float(input("Tell me the number of years"))
-print(f'The are {round(years*365*24*3600*3*10**8,0)} meters in {years} light years')
+print(f'The are {int(years*365*24*3600*3*10**8)} meters in {years} light years')
