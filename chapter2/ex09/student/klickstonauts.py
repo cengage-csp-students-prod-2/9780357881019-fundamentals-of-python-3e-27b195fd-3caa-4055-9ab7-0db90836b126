083ac8fa7752c267858e9f00ec871059c9d424c9
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-20 17:40:00 +0000

    Testing Mon Jan 20 2025 17:40:00 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex09/student/klickstonauts.py
+++ chapter2/ex09/student/klickstonauts.py
@@ -1,3 +1,3 @@
 # Write your program here
 km=float(input("Tell me the number of kilometers"))
-print(f'{km} kms is {round(km*90*60/10000,1)} nautical miles')
+print(f'{km} kms is {km*90*60/10000} nautical miles')
