0b7432f1efbeb420ceddef78b52c1997214eff91
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-20 16:08:23 +0000

    Testing Mon Jan 20 2025 16:08:23 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex04/student/sphere.py
+++ chapter2/ex04/student/sphere.py
@@ -1,4 +1,7 @@
 # Write your program here
 from math import pi
 radius=float(input("Tell me the radius of the sphere"))
-print(f'The volume is {radius**3*4*pi/3}')
+print(f'The diameter is {radius*2} units')
+print(f'The circumference is {radius*2*pi} units')
+print(f'The surface area is {radius**2*pi*4} square units')
+print(f'The volume is {radius**3*4*pi/3} cubic units')
