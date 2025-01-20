1ab61849ac5b8077af96894dcd6d1287b2652428
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-20 17:08:10 +0000

    Testing Mon Jan 20 2025 17:08:10 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/momentum.py
+++ chapter2/ex06/student/momentum.py
@@ -1,1 +1,5 @@
 # Write your program here
+mass=float(input("Tell me the mass"))
+velocity=float(input("Tell me the velocity"))
+print(f'The impulse is {mass*velocity}')
+print(f'The energy is {0.5*mass*velocity**2}')
