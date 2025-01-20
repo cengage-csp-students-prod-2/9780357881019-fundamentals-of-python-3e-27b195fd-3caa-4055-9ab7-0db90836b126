cadfb2829e8a4ec1b16aaf74df99ce1e277d9293
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-20 15:36:56 +0000

    Testing Mon Jan 20 2025 15:36:56 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/cube.py
+++ chapter2/ex02/student/cube.py
@@ -1,3 +1,3 @@
 # Write your program here
 cubeedge=float(input("Tell me the length of the edge of the cube"))
-print(f'The surface of the cube is {cubeedge**2*6}')
+print(f'The surface of the cube is {cubeedge**2*6} square units')
