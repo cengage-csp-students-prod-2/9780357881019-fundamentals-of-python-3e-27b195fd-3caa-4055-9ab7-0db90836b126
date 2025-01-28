ef429b26c112b508f787939fab05c6340072b399
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-28 23:12:24 +0000

    Testing Tue Jan 28 2025 23:12:24 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex01/student/equilateral.py
+++ chapter3/ex01/student/equilateral.py
@@ -2,4 +2,4 @@
 a=float(input("enter side 1"))
 b=float(input("enter side 2"))
 c=float(input("enter side 3"))
-print(True if a==b==c else False)
+print("triangle is equilateral" if a==b==c else "triangle is not equilateral")
