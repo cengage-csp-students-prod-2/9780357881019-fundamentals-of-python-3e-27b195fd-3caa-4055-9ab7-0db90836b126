fdbf84f6fcb37a600a7bf347a829a9cab6543347
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-28 23:29:08 +0000

    Testing Tue Jan 28 2025 23:29:08 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex02/student/right.py
+++ chapter3/ex02/student/right.py
@@ -1,2 +1,7 @@
 # Write your program here
 print('Hello')
+a=float(input("enter side 1"))
+b=float(input("enter side 2"))
+c=float(input("enter side 3"))
+sides=[a,b,c].sorted()
+print(True if a**2+b**2==c**2 else False)
