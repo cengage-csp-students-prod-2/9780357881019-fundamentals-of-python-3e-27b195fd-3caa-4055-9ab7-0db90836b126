d86c950bf1c520fc3a9f2cbbe567fea207135dab
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-28 23:42:55 +0000

    Testing Tue Jan 28 2025 23:42:55 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex04/student/bouncy.py
+++ chapter3/ex04/student/bouncy.py
@@ -1,1 +1,6 @@
 # Write your program here
+a=float(input("Enter the height from which the ball is dropped: "))
+bi=float(input("Enter the bounciness index of the ball: "))
+n=float(input("Enter the number of times the ball is allowed to continue bouncing: "))
+d=a*bi**n+a*bi**(n-1)
+print(f'Total distance traveled is: {d} units.')
