0e762adc6762c75340850a1f9e9a5a69bb69440e
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-01-29 00:42:14 +0000

    Testing Wed Jan 29 2025 00:42:14 GMT+0000 (Coordinated Universal Time)

--- chapter3/ex06/student/leibniz.py
+++ chapter3/ex06/student/leibniz.py
@@ -3,6 +3,6 @@ it=int(input('tell me the number of iterations '))
 p=0
 sign=1
 for i in range(it):
-    p+=sign/(1+2*it)
+    p+=sign/(1+2*i)
     sign*=-1
 print(f'The approximation of pi is {p*4}')
