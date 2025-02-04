fcdc865c43e83e048d774cf20f2ecf2ee3a1d21f
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-04 23:23:14 +0000

    Testing Tue Feb 04 2025 23:23:14 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/octaltodecimal.py
+++ chapter4/ex04/student/octaltodecimal.py
@@ -1,1 +1,8 @@
 # Write your program here
+inf=input("input file")
+#outf=input("outputfile")
+#with open(inf, 'r') as f:
+#    line=int(f.read())
+#with open(outf,'w') as f:
+#    f.write(format(line,'o'))
+print(format(int(inf),'o'))
