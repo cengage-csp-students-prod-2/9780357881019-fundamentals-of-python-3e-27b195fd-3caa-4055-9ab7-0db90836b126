69160a21e58f9124e27bb45d332bade7acb67965
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-04 23:26:57 +0000

    Testing Tue Feb 04 2025 23:26:57 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/octaltodecimal.py
+++ chapter4/ex04/student/octaltodecimal.py
@@ -1,8 +1,15 @@
 # Write your program here
 inf=input("input file")
+inf=list(inf)
+n=0
+m=1
+while inf:
+    n+=inf.pop()*m
+    m*=8
+print(n)
 #outf=input("outputfile")
 #with open(inf, 'r') as f:
 #    line=int(f.read())
 #with open(outf,'w') as f:
 #    f.write(format(line,'o'))
-print(format(int(inf),'o'))
+#print(format(int(inf),'o'))
