73660367c7186dada602c0c9278871b108876fbf
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-04 23:20:47 +0000

    Testing Tue Feb 04 2025 23:20:47 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/decimaltooctal.py
+++ chapter4/ex04/student/decimaltooctal.py
@@ -1,1 +1,7 @@
 # Write your program here
+infname=input("input file")
+outf=input("outputfile")
+with open(infname, 'r') as f:
+    line=int(f.read())
+with open(outf,'w') as f:
+    f.write(format(line,'o'))
