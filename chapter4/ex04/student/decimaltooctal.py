d60014dcd7327a22af36a9c2580dd43d60e52b9c
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-04 23:22:29 +0000

    Testing Tue Feb 04 2025 23:22:29 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/decimaltooctal.py
+++ chapter4/ex04/student/decimaltooctal.py
@@ -1,7 +1,8 @@
 # Write your program here
-infname=input("input file")
-outf=input("outputfile")
-with open(infname, 'r') as f:
-    line=int(f.read())
-with open(outf,'w') as f:
-    f.write(format(line,'o'))
+inf=input("input file")
+#outf=input("outputfile")
+#with open(inf, 'r') as f:
+#    line=int(f.read())
+#with open(outf,'w') as f:
+#    f.write(format(line,'o'))
+print(format(int(inf),'o'))
