0fe276792d621d337ef980e89bd61d5d25d6690c
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-06 22:18:43 +0000

    Testing Thu Feb 06 2025 22:18:43 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex01/student/stats.py
+++ chapter5/ex01/student/stats.py
@@ -1,1 +1,24 @@
 # Write your program here
+def median(mlist:list):
+    return sorted(mlist)[len(mlist)//2] if mlist else 0
+def mean(mlist:list):
+    return sum(mlist)/len(mlist) if mlist else 0
+def mode(mlist:list):
+    if not mlist return 0
+    d={}
+    maxn=0
+    for n in mlist:
+        if n in d:
+            d[n]+=1
+        else:
+            d[n]=1
+        if d[n]>maxn:
+            maxn=d[n]
+            ni=n
+
+    return ni
+
+def main(mlist:list):
+    print(median(mlist))
+    print(mean(mlist))
+    print(mode(mlist))
