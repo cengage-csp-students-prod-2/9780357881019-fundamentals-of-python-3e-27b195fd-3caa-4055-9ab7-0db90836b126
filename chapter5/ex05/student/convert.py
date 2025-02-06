44fe5f2e830f83b7eadd014abcb54fc14da05b13
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-06 23:29:53 +0000

    Testing Thu Feb 06 2025 23:29:53 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex05/student/convert.py
+++ chapter5/ex05/student/convert.py
@@ -1,1 +1,15 @@
 # Write your program here
+DIGITS={}
+for i in range(10): DIGITS[i]=i
+for i in range(26): DIGITS[chr(65+i)]=i+10
+def repToDecimal (rep,base):
+    rep2=rep.upper()
+    l=list(rep)
+    n=0
+    m=1
+    while l:
+        n+=DIGITS[l.pop()]*m
+        m*=base
+    return n
+def main(s):
+    print(repToDecimal(s))
