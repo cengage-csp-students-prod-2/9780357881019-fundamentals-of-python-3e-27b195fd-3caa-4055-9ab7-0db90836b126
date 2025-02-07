ec3bd6907f1bf2023aec034323a6015842a6226e
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-07 04:41:55 +0000

    Testing Fri Feb 07 2025 04:41:55 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex01/student/newton.py
+++ chapter7/ex01/student/newton.py
@@ -1,1 +1,17 @@
 # Write your code here
+TOLERANCE=0.1
+def limitReached (x,appr):
+    if abs(x-appr**2)<TOLERANCE: return True
+    return False
+def improveEstimate(x,estimate):
+    return (estimate + x / estimate) / 2
+def newton(x:float,estimate:float=1):
+    estimate=improveEstimate(x,estimate)
+    if limitReached(x,estimate): return estimate
+    return newton(x,estimate)
+
+x=1
+while x>0:
+    x=float(input("Enter a positive number or enter/return to quit: ")
+    if x<=0: break
+    print("The program's estimate is ", newton(x))
