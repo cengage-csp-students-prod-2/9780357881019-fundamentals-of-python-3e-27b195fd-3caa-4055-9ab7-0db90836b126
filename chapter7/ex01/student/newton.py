0f9ea2a57db35dcb68a8e956562aa3c1a0049de3
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-07 04:48:47 +0000

    Testing Fri Feb 07 2025 04:48:47 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex01/student/newton.py
+++ chapter7/ex01/student/newton.py
@@ -9,11 +9,11 @@ def newton(x:float,estimate:float=1):
     estimate=improveEstimate(x,estimate)
     if limitReached(x,estimate): return estimate
     return newton(x,estimate)
-
-x=1
-while x>0:
-    x=input("Enter a positive number or enter/return to quit: ")
-    if x=="": break
-    x=float(x)
-    if x<=0: break
-    print("The program's estimate is ", newton(x))
+def main():
+    x=1
+    while x>0:
+        x=input("Enter a positive number or enter/return to quit: ")
+        if x=="": break
+        x=float(x)
+        if x<=0: break
+        print("The program's estimate is ", newton(x))
