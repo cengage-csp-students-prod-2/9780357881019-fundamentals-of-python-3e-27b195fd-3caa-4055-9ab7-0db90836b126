3c6081f90cad99eebebd96e86cd57d40c8bd51b5
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-07 01:27:09 +0000

    Testing Fri Feb 07 2025 01:27:09 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex06/student/testmyrange.py
+++ chapter6/ex06/student/testmyrange.py
@@ -1,1 +1,19 @@
 # Write your code here
+def myRange(start:int,stop:int=None,step:int=None)
+    if stop is None:
+        if step<0 and start>0:
+            stop=0
+        start,stop=0,start
+    if step is None or step=0:
+        step=1
+    l=[]
+    pos=start
+    if step>0:
+        while pos<stop:
+            l.append(pos)
+            pos+=step
+    else:
+        while pos>stop:
+            l.append(pos)
+            pos+=step
+    return l
