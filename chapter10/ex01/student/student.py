e5e21b716a79168fe823842f26552d496212bf20
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-09 03:19:42 +0000

    Testing Sun Feb 09 2025 03:19:42 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex01/student/student.py
+++ chapter10/ex01/student/student.py
@@ -1,1 +1,27 @@
 # Write your code here
+class Student():
+    def __init__(self, name):
+        self.name = name
+    def __eq__(self, other):
+        return self.name == other.name
+    def __lt__(self, other):
+        return self.name < other.name
+    # def __le__(self, other):
+    #     return self.name <= other.name
+    # def __gt__(self, other):
+    #     return self.name > other.name
+    def __ge__(self, other):        
+        return self.name >= other.name
+    
+def main():
+    s1 = Student("John")
+    s2 = Student("Mike")
+    print(s1 == s2,": ",s1.name == s2.name)
+    print(s1 < s2,": ",s1.name < s2.name)
+    print(s1 <= s2,": ",s1.name <= s2.name)
+    print(s1 > s2,": ",s1.name > s2.name)
+    print(s1 >= s2,": ",s1.name >= s2.name)
+    print(s1 != s2,": ",s1.name != s2.name)
+
+if __name__ == "__main__":
+    main()
