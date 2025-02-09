7d119e104e72b8d27970168abf4c4fddd46633b3
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-09 01:19:55 +0000

    Testing Sun Feb 09 2025 01:19:55 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex01/student/taxformwithgui.py
+++ chapter9/ex01/student/taxformwithgui.py
@@ -1,1 +1,41 @@
 # Write your code here
+# Initialize the constants
+import breezypythongui
+
+               
+import time
+
+# Compute the income tax
+class Window(breezypythongui.EasyFrame):
+    TAX_RATE = 0.20
+    STANDARD_DEDUCTION = 10000.0
+    DEPENDENT_DEDUCTION = 3000.0
+    
+    
+    def __init__(self):
+        breezypythongui.EasyFrame.__init__(self, "Tax Calculator")
+        self.addLabel("Gross Income", 0, 0)
+        self.addLabel("Dependants", 1, 0)
+        self.dependants=self.addIntegerField(0, 1, 1)
+        self.gross=self.addFloatField(0, 0, 1)
+        self.button=self.addButton("Compute", 2, 0,command= self.gettax)
+        self.addLabel("Total tax", 3, 0)
+        self.taxfield=self.addTextField(0, 3, 1,state="readonly")
+
+    def gettax(self):
+        try:
+            gross=self.gross.getNumber()
+        except ValueError:
+            self.messageBox("Error", "Enter a number for gross income")
+            return
+        try:
+            dependants=self.dependants.getNumber()
+        except ValueError:
+            self.messageBox("Error", "Enter a number for dependants")
+            return 
+        tax= (gross - self.STANDARD_DEDUCTION - self.DEPENDENT_DEDUCTION * dependants )*self.TAX_RATE
+        self.taxfield.setValue("$"+format(tax, '.2f'))
+
+
+
+Window().mainloop()
