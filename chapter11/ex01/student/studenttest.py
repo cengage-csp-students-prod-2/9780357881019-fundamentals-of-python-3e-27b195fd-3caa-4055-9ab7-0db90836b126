30b2318340e4b3a42bca7021921accae6f1d3ebc
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-11 04:02:34 +0000

    Testing Tue Feb 11 2025 04:02:34 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex01/student/studenttest.py
+++ chapter11/ex01/student/studenttest.py
@@ -1,1 +1,21 @@
 # Write your code here
+from student import Student
+
+def main():
+    student = Student("Ken", 10)
+    student.randomizeScores(10, 75, 100)
+    print(student)
+    print("Mean:", student.getMean())
+    print("Median:", student.getMedian())
+    print("Mode:", student.getMode())
+    print("Standard deviation:", student.getStd())
+    student.randomizeScores(10, 50, 100)
+    print(student)
+    print("Mean:", student.getMean())
+    print("Median:", student.getMedian())
+    print("Mode:", student.getMode())
+    print("Standard deviation:", student.getStd())
+
+
+if __name__ == "__main__":
+    main()
