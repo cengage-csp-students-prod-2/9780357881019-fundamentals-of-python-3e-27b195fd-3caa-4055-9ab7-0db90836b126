d1df38e51bbdc3cc12f0061a9976a7e74c7b6c96
StarcatcherDKpublic <mehtoc108@gmail.com>
2025-02-11 04:07:08 +0000

    Testing Tue Feb 11 2025 04:07:08 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex01/student/studenttest.py
+++ chapter11/ex01/student/studenttest.py
@@ -15,6 +15,30 @@ def main():
     print("Median:", student.getMedian())
     print("Mode:", student.getMode())
     print("Standard deviation:", student.getStd())
+    n=int(input("Enter action:\n1. Add score\n2. Delete score\n3. Replace score\n4. Exit\n"))
+    while n!=4:
+        if n==1:
+            student.addScore(int(input("Enter score: ")))
+            print(student)
+            print("Mean:", student.getMean())
+            print("Median:", student.getMedian())
+            print("Mode:", student.getMode())
+            print("Standard deviation:", student.getStd())
+        elif n==2:
+            student.deleteScore(int(input("Enter index: ")))
+            print(student)
+            print("Mean:", student.getMean())
+            print("Median:", student.getMedian())
+            print("Mode:", student.getMode())
+            print("Standard deviation:", student.getStd())
+        elif n==3:
+            student.setScore(int(input("Enter index: ")), int(input("Enter score: ")))
+            print(student)
+            print("Mean:", student.getMean())
+            print("Median:", student.getMedian())
+            print("Mode:", student.getMode())
+            print("Standard deviation:", student.getStd())
+        n=int(input("Enter action:\n1. Add score\n2. Delete score\n3. Replace score\n4. Exit\n"))
 
 
 if __name__ == "__main__":
